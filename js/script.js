900fccf1a43688330286da34af668108a41f9f9a
MuhammadRovendra <muhammad_rovendra@teknokrat.ac.id>
2023-05-24 23:32:02 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function(){
+
+        $('#menu').toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+
+    });
+
+});
